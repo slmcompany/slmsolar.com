fa28b3737d8f695ccc13fed9d2ece5ff9f294609
thanhtrang16490 <31572762+thanhtrang16490@users.noreply.github.com>
2025-02-25 16:16:48 +0700

    update 25022025-lần 7

--- src/components/home/whychoisesolarmax.tsx
+++ src/components/home/whychoisesolarmax.tsx
@@ -18,7 +18,7 @@ const WhyChooseSolarmax = () => {
           <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" />
         </svg>
       ),
-      text: "Tư Vấn Miễn Phí",
+      text: "Tư Vấn & Khảo Sát miễn Phí",
       href: "#",
       background: "https://images.unsplash.com/photo-1508514177221-188b1cf16e9d?q=80",
       content: {
@@ -61,7 +61,7 @@ const WhyChooseSolarmax = () => {
           <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75a4.5 4.5 0 01-4.884 4.484c-1.076-.091-2.264.071-2.95.904l-7.152 8.684a2.548 2.548 0 11-3.586-3.586l8.684-7.152c.833-.686.995-1.874.904-2.95a4.5 4.5 0 016.336-4.486l-3.276 3.276a3.004 3.004 0 002.25 2.25l3.276-3.276c.256.565.398 1.192.398 1.852z" />
         </svg>
       ),
-      text: "Bảo Hành 24/7",
+      text: "Bảo Hành & Hỗ Trợ 24/7",
       href: "#",
       background: "https://images.unsplash.com/photo-1542336391-ae2936d8af02?q=80",
       content: {
